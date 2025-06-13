43340a6dd3877638b97194527d63b715374f5604
rahulpoddar1 <rahulpoddar1000@gmail.com>
2025-06-13 18:33:36 +0545

    User Model Add

---
+++ src/Models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email:    { type: String, required: true, unique: true },
+  firstName:{ type: String, required: true },
+  lastName: { type: String, required: true },
+  role:     { type: String, enum: ["user", "admin"], required: true },
+  password: { type: String, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
